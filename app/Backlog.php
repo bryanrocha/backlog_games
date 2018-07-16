97504d911b5619fcb716fe4df5b26023c044da74
bryan rocha <rocha.bryan@gmail.com>
2018-07-16 18:56:54 -0300

    nova relação na classe Backlog

--- app/Backlog.php
+++ app/Backlog.php
@@ -11,4 +11,8 @@ class Backlog extends Model
     public function user(){
     	return $this->belongsTo('App\User');
     }
+
+    public function game(){
+    	return $this->belongsTo('App\Game');
+    }
 }
